0217c049310b8951a25241e5067774cf39351d2b
Prateek jain <prateek.jn624@gmail.com>
2022-11-19 13:29:31 +0530

    [prateek]-Compute a*b+c

--- SortingArithmaticComputation.sh
+++ SortingArithmaticComputation.sh
@@ -4,5 +4,5 @@ read -p "Enter the first value:" a
 read -p "Enter the second value:" b
 read -p "Enter the third value:" c
 
-sum=$(( $a + $b * $c ))
+sum=$(( $a * $b + $c ))
 echo "the sum is :$sum"
