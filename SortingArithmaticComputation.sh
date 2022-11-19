180ca8e55990703d9ecb0c5ed82517f32fcaae50
Prateek jain <prateek.jn624@gmail.com>
2022-11-19 11:44:33 +0530

    [prateek]-Taking inputs from user

--- SortingArithmaticComputation.sh
+++ SortingArithmaticComputation.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-echo Welcome to Sorting Arithmatic Computation.
+read -p "Enter the first value:" a
+read -p "Enter the second value:" b
+read -p "Enter the third value:" c
+
+echo "the value entered by user are $a $b $c"
