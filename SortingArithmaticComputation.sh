9bcb2ff7a27048355fcf61f6a79e58c2453f8888
Prateek jain <prateek.jn624@gmail.com>
2022-11-20 13:16:09 +0530

    [prateek]-results in dictionary

--- SortingArithmaticComputation.sh
+++ SortingArithmaticComputation.sh
@@ -4,5 +4,16 @@ read -p "Enter the first value:" a
 read -p "Enter the second value:" b
 read -p "Enter the third value:" c
 
-sum=$(( $a % $b + $c ))
-echo "the sum is :$sum"
+sum1=$(( $a + $b * $c ))
+sum2=$(( $a * $b + $c ))
+sum3=$(( $c + $a / $b ))
+sum4=$(( $a % $b + $c ))
+
+declare -A dict
+dict=(["a"]=$sum1 ['b']=$sum2 ['c']=$sum3 ['d']=$sum4)
+
+for i in "${!dict[@]}"
+do
+        echo "$i"="${dict[$i]}"
+done
+
